30573e247f1d88675c64f6cc0ecd44cf1dc00e43
RUTHWIK G <166355978+Ruthwik008@users.noreply.github.com>
2026-01-12 22:32:04 +0530

    1

---
+++ knapsack_bottomup.java
@@ -0,0 +1,37 @@
+public class knapsack_bottomup {
+
+    public static int knapsack(int[] wt, int[] val, int W) {
+        int n = wt.length;
+
+        // DP table
+        int[][] dp = new int[n + 1][W + 1];
+
+        // Build table dp[][] in bottom-up manner
+        for (int i = 1; i <= n; i++) {
+            for (int w = 1; w <= W; w++) {
+
+                if (wt[i - 1] <= w) {
+                    dp[i][w] = Math.max(
+                            val[i - 1] + dp[i - 1][w - wt[i - 1]],
+                            dp[i - 1][w]
+                    );
+                } else {
+                    dp[i][w] = dp[i - 1][w];
+                }
+            }
+        }
+
+        return dp[n][W];
+    }
+
+    public static void main(String[] args) {
+        int[] wt = {10, 20, 30};
+        int[] val = {60, 100, 120};
+        int W = 50;
+
+        System.out.println(knapsack(wt, val, W));
+    }
+}
+ {
+    
+}
