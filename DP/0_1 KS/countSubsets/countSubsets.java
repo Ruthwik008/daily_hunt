751a6bc321993cc4ba7492dc190746e626080425
RUTHWIK G <166355978+Ruthwik008@users.noreply.github.com>
2026-02-22 12:42:32 +0530

    finally after days

---
+++ DP/0_1 KS/countSubsets/countSubsets.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    public static int countSubsets(int[] arr, int n, int sum) {
+
+        // Base cases
+        if (sum == 0) return 1;   // found one subset
+        if (n == 0) return 0;     // no elements left
+
+        if (arr[n - 1] > sum)
+            return countSubsets(arr, n - 1, sum);
+
+        return countSubsets(arr, n - 1, sum - arr[n - 1]) +
+               countSubsets(arr, n - 1, sum);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 6, 8, 10};
+        int sum = 10;
+
+        System.out.println(countSubsets(arr, arr.length, sum));
+    }
+}
